228511aedd6c0dd8a52685ba08fc9d1d0d1f3494
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-04-16 10:22:02 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
 import { ThemeProvider } from "./components/ThemeProvider";
 
-//pages
-import Home from "./pages/Home";
-import OurStory from "./pages/OurStory";
-
 //components
-import Footer from "./components/Footer/Footer";
-import Hero from "./components/Hero";
+import AppRoutes from "./components/AppRoutes";
 
 const App = () => {
   return (
     <>
       <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
-       
+        <AppRoutes />
       </ThemeProvider>
     </>
   );
