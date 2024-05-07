4a96195d4af04b0bfc4fc58a876f5a59450dd29e
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-05-07 18:56:22 +0530

    first commit

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -12,18 +12,19 @@ import { toast } from "@/components/ui/use-toast";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import PhoneInput from 'react-phone-input-2';
-import 'react-phone-input-2/lib/style.css';
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 import { useState } from "react";
 
 const EnquireFormSchema = z.object({
-  FirstName: z.string().min(5).max(20),
-  LastName: z.string().min(5).max(30),
-  Address: z.string(),
-  PhoneNo: z.number().min(1).max(10),
-  Age: z.number().min(18),
-  Country: z.string().min(1),
-  Text: z.string().min(1),
+  FirstName: z.string().min(5, "First name should be at least 5 words longer"),
+  LastName: z.string().min(5, "Last name should be at least 5 words longer"),
+  Address: z.string().min(1, "Address cannot be empty"),
+  PhoneNo: z.string().min(1).max(10),
+  Age: z.number().min(18, "should be higher than 18"),
+  Country: z.string().optional(),
+  Text: z.string().optional(),
+  Email: z.string(),
 });
 
 const Contact = () => {
@@ -36,6 +37,7 @@ const Contact = () => {
       Text: "",
       Age: 0,
       Country: "",
+      Email: "@gmail.com",
     },
   });
 
@@ -45,20 +47,21 @@ const Contact = () => {
     });
   };
 
-  const [value, setValue] = useState('in');
+  const [value, setValue] = useState("in");
 
   return (
     <main className="bg-[#E8B587A1] p-8 flex flex-col gap-10">
       <h1 className="island text-4xl md:text-7xl text-yellow-700 px-8">
         Enquire Now
       </h1>
-      <div className="container w-3/4 py-8 border-white border-2 rounded-lg shadow-md">
+      <div className="container w-1/2 py-8 border-white border-2 rounded-lg shadow-md">
         <Form {...form}>
           <form
-            className="w-full space-y-7"
+            className="w-full space-y-7 grid"
             onSubmit={form.handleSubmit(onSubmit)}
           >
-            <div className="flex items-center justify-evenly">
+            {/* first and last name */}
+            <div className="grid grid-cols-2 gap-8">
               <FormField
                 control={form.control}
                 name="FirstName"
@@ -94,12 +97,59 @@ const Contact = () => {
                 )}
               />
             </div>
-            <PhoneInput
-              country={value}
-              placeholder="Enter your phone number"
-              onChange={(country) => {setValue(country)}}
-              countryCodeEditable={false}
-            />
+
+            {/* phone number, email and address     */}
+            <div className="grid grid-cols-2 grid-rows-2 gap-8">
+              <div className="grid gap-5">
+                <FormField
+                  control={form.control}
+                  name="Email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="font-semibold">Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="border border-neutral-500 w-full shadow-inner shadow-black"
+                          placeholder=""
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="PhoneNo"
+                  render={() => (
+                    <FormItem className="w-full rounded-md">
+                      <FormLabel className="font-semibold">Phone No.</FormLabel>
+                      <FormControl>
+                        {/* <Input
+                          className="border border-neutral-500 w-full shadow-inner shadow-black"
+                          placeholder=""
+                          {...field}
+                        /> */}
+                        <PhoneInput
+                          country={value}
+                          placeholder="Enter your phone number"
+                          onChange={(country) => {
+                            setValue(country);
+                          }}
+                          countryCodeEditable={false}
+                          inputClass="border border-neutral-500 w-full shadow-inner shadow-black w-max"
+                          containerClass="w-full"
+                          buttonClass="border border-neutral-500"
+                          
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </div>
             <Button
               className="bg-[#B26C02] font-semibold shadow-md transition-all hover:bg-[#B26C02]/70 w- p-8 text-lg tracking-wid uppercase"
               type="submit"
