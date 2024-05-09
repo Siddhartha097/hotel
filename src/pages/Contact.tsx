fbb27c38388de97e281270700a44e70d2b4ddcaf
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-05-09 16:06:37 +0530

    first commit

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,25 +1,7 @@
-// import { Button } from "@/components/ui/button";
-// import {
-//   Form,
-//   FormControl,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form";
-// import { Input } from "@/components/ui/input";
-// import { toast } from "@/components/ui/use-toast";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
-// import { z } from "zod";
-// import PhoneInput from "react-phone-input-2";
-// import "react-phone-input-2/lib/style.css";
-// import { useState } from "react";
-// import { Textarea } from "@/components/ui/textarea";
+
 import 'react-phone-input-2/lib/style.css'
 import EnquireForm from "@/components/EnquireForm";
 
-// const EnquireFormSchema = z.object({
 //   FirstName: z.string().min(5, "First name should be at least 5 words longer"),
 //   LastName: z.string().min(5, "Last name should be at least 5 words longer"),
 //   Address: z.string().min(1, "Address cannot be empty"),
@@ -31,26 +13,6 @@ import EnquireForm from "@/components/EnquireForm";
 // });
 
 const Contact = () => {
-  // const form = useForm<z.infer<typeof EnquireFormSchema>>({
-  //   resolver: zodResolver(EnquireFormSchema),
-  //   defaultValues: {
-  //     FirstName: "",
-  //     LastName: "",
-  //     Address: "",
-  //     Text: "",
-  //     Age: 0,
-  //     Country: "",
-  //     Email: "@gmail.com",
-  //   },
-  // });
-
-  // const onSubmit = () => {
-  //   toast({
-  //     title: "Enquiry Sent Successfully",
-  //   });
-  // };
-
-  // const [value, setValue] = useState("in");
 
   return (
     <main className="bg-[#E8B587A1] p-8 flex flex-col gap-10">
