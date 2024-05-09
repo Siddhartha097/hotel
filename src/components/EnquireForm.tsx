7433a62eac00364d87853f0de1ccc61b7fcf6c69
Siddhartha Banerjee <59687592+Siddhartha097@users.noreply.github.com>
2024-05-09 16:05:05 +0530

    first commit

---
+++ src/components/EnquireForm.tsx
@@ -0,0 +1,242 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+// import { toast } from "./ui/use-toast";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form";
+import { Input } from "./ui/input";
+import PhoneInput from "react-phone-input-2";
+import { Textarea } from "./ui/textarea";
+import ReactFlagsSelect from "react-flags-select";
+
+const EnquireFormSchema = z.object({
+  FirstName: z.string().min(5, "First name should be at least 5 words longer"),
+  LastName: z.string().min(5, "Last name should be at least 5 words longer"),
+  Address: z.string().min(1, "Address cannot be empty"),
+  PhoneNo: z.string().min(1).max(13),
+  Age: z.string().optional(),
+  Country: z.string().optional(),
+  Text: z.string().optional(),
+  Email: z.string(),
+});
+
+const EnquireForm = () => {
+  const form = useForm<z.infer<typeof EnquireFormSchema>>({
+    resolver: zodResolver(EnquireFormSchema),
+    defaultValues: {
+      FirstName: "",
+      LastName: "",
+      Address: "",
+      Text: "",
+      Age: "",
+      Country: "",
+      Email: "@gmail.com",
+    },
+  });
+
+  const onSubmit = (data: z.infer<typeof EnquireFormSchema>) => {
+    // toast({
+    //   title: "Enquiry Sent Successfully",
+    // });
+    console.log(JSON.stringify(data));
+    
+  };
+
+//   const [value, setValue] = useState("in");
+
+  const [selected, setSelected] = useState("");
+
+  return (
+    <div className="container w-1/2 py-8 border-white border-2 rounded-lg shadow-md">
+      <Form {...form}>
+        <form
+          className="w-full space-y-7 grid"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          {/* first and last name */}
+          <div className="grid grid-cols-2 gap-8">
+            <FormField
+              control={form.control}
+              name="FirstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-semibold">First Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="border border-neutral-500 w-full shadow-inner shadow-black"
+                      placeholder=""
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="LastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-semibold">Last Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="border border-neutral-500 shadow-inner shadow-black"
+                      placeholder=""
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          {/* phone number, email and address     */}
+          <div className="grid grid-cols-2 gap-8">
+            <div className="grid gap-5">
+              <FormField
+                control={form.control}
+                name="Email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="font-semibold">Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="border border-neutral-500 w-full shadow-inner shadow-black row-span-1"
+                        placeholder=""
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="PhoneNo"
+                render={({ field }) => (
+                  <FormItem className="w-full rounded-md">
+                    <FormLabel className="font-semibold">Phone No.</FormLabel>
+                    <FormControl>
+                      <PhoneInput
+                        country="in"
+                        placeholder="Enter your phone number"
+                        countryCodeEditable={false}
+                        inputClass=""
+                        buttonClass="border border-neutral-500"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <FormField
+              control={form.control}
+              name="Address"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-semibold">Address</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      className="border border-neutral-500 h-full p-3 shadow-inner shadow-black resize-none"
+                      placeholder=""
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          {/* age and country */}
+          <div className="grid grid-cols-2 gap-8 pt-5">
+            <FormField
+              control={form.control}
+              name="Age"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-semibold">Age</FormLabel>
+                  <FormControl>
+                    <Input
+                      className="border border-neutral-500 w-full shadow-inner shadow-black"
+                      placeholder=""
+                      {...field}
+                      type="number"
+                      max={100}
+                      min={1}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="Country"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="font-semibold">Country</FormLabel>
+                  <FormControl>
+                    {/* <Input
+                      className="border border-neutral-500 shadow-inner shadow-black"
+                      placeholder=""
+                      {...field}
+                    /> */}
+                    <ReactFlagsSelect
+                      selected={selected}
+                      onSelect={(code) => setSelected(code)}
+                      {...field}
+                      className="border border-neutral-500 shadow-inner shadow-black bg-white rounded-lg menu-flags"
+                      searchable
+                      searchPlaceholder="search countries"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          {/* text */}
+          <FormField
+            control={form.control}
+            name="Text"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="font-semibold">Text</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className="border row-span-2 border-neutral-500 w-full shadow-inner shadow-black resize-none"
+                    placeholder=""
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            className="bg-[#B26C02] font-semibold shadow-md transition-all hover:bg-[#B26C02]/70 w- p-8 text-lg tracking-wid uppercase"
+            type="submit"
+          >
+            Enquire Now
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
+
+export default EnquireForm;
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,238 +1,63 @@
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { toast } from "@/components/ui/use-toast";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css";
-import { useState } from "react";
-import { Textarea } from "@/components/ui/textarea";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormField,
+//   FormItem,
+//   FormLabel,
+//   FormMessage,
+// } from "@/components/ui/form";
+// import { Input } from "@/components/ui/input";
+// import { toast } from "@/components/ui/use-toast";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { z } from "zod";
+// import PhoneInput from "react-phone-input-2";
+// import "react-phone-input-2/lib/style.css";
+// import { useState } from "react";
+// import { Textarea } from "@/components/ui/textarea";
+import 'react-phone-input-2/lib/style.css'
+import EnquireForm from "@/components/EnquireForm";
 
-const EnquireFormSchema = z.object({
-  FirstName: z.string().min(5, "First name should be at least 5 words longer"),
-  LastName: z.string().min(5, "Last name should be at least 5 words longer"),
-  Address: z.string().min(1, "Address cannot be empty"),
-  PhoneNo: z.string().min(1).max(10),
-  Age: z.number().min(18, "should be higher than 18"),
-  Country: z.string().optional(),
-  Text: z.string().optional(),
-  Email: z.string(),
-});
+// const EnquireFormSchema = z.object({
+//   FirstName: z.string().min(5, "First name should be at least 5 words longer"),
+//   LastName: z.string().min(5, "Last name should be at least 5 words longer"),
+//   Address: z.string().min(1, "Address cannot be empty"),
+//   PhoneNo: z.string().min(1).max(10),
+//   Age: z.number().min(18, "should be higher than 18"),
+//   Country: z.string().optional(),
+//   Text: z.string().optional(),
+//   Email: z.string(),
+// });
 
 const Contact = () => {
-  const form = useForm<z.infer<typeof EnquireFormSchema>>({
-    resolver: zodResolver(EnquireFormSchema),
-    defaultValues: {
-      FirstName: "",
-      LastName: "",
-      Address: "",
-      Text: "",
-      Age: 0,
-      Country: "",
-      Email: "@gmail.com",
-    },
-  });
+  // const form = useForm<z.infer<typeof EnquireFormSchema>>({
+  //   resolver: zodResolver(EnquireFormSchema),
+  //   defaultValues: {
+  //     FirstName: "",
+  //     LastName: "",
+  //     Address: "",
+  //     Text: "",
+  //     Age: 0,
+  //     Country: "",
+  //     Email: "@gmail.com",
+  //   },
+  // });
 
-  const onSubmit = () => {
-    toast({
-      title: "Enquiry Sent Successfully",
-    });
-  };
+  // const onSubmit = () => {
+  //   toast({
+  //     title: "Enquiry Sent Successfully",
+  //   });
+  // };
 
-  const [value, setValue] = useState("in");
+  // const [value, setValue] = useState("in");
 
   return (
     <main className="bg-[#E8B587A1] p-8 flex flex-col gap-10">
       <h1 className="island text-4xl md:text-7xl text-yellow-700 px-8">
         Enquire Now
       </h1>
-      <div className="container w-1/2 py-8 border-white border-2 rounded-lg shadow-md">
-        <Form {...form}>
-          <form
-            className="w-full space-y-7 grid"
-            onSubmit={form.handleSubmit(onSubmit)}
-          >
-            {/* first and last name */}
-            <div className="grid grid-cols-2 gap-8">
-              <FormField
-                control={form.control}
-                name="FirstName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-semibold">First Name</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="border border-neutral-500 w-full shadow-inner shadow-black"
-                        placeholder=""
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="LastName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-semibold">Last Name</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="border border-neutral-500 shadow-inner shadow-black"
-                        placeholder=""
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            {/* phone number, email and address     */}
-            <div className="grid grid-cols-2 gap-8">
-              <div className="grid gap-5">
-                <FormField
-                  control={form.control}
-                  name="Email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="font-semibold">Email</FormLabel>
-                      <FormControl>
-                        <Input
-                          className="border border-neutral-500 w-full shadow-inner shadow-black row-span-1"
-                          placeholder=""
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-
-                <FormField
-                  control={form.control}
-                  name="PhoneNo"
-                  render={() => (
-                    <FormItem className="w-full rounded-md">
-                      <FormLabel className="font-semibold">Phone No.</FormLabel>
-                      <FormControl>
-                        {/* <Input
-                          className="border border-neutral-500 w-full shadow-inner shadow-black"
-                          placeholder=""
-                          {...field}
-                        /> */}
-                        <PhoneInput
-                          country={value}
-                          placeholder="Enter your phone number"
-                          onChange={(country) => {
-                            setValue(country);
-                          }}
-                          countryCodeEditable={false}
-                          inputClass="border border-neutral-500 w-full shadow-inner shadow-black w-max"
-                          containerClass="w-full"
-                          buttonClass="border border-neutral-500"
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <FormField
-                control={form.control}
-                name="Address"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-semibold">Address</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        className="border row-span-2 border-neutral-500 w-full shadow-inner shadow-black"
-                        placeholder=""
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            {/* age and country */}
-            <div className="grid grid-cols-2 gap-8">
-              <FormField
-                control={form.control}
-                name="Age"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-semibold">Age</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="border border-neutral-500 w-full shadow-inner shadow-black"
-                        placeholder=""
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="Country"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-semibold">Country</FormLabel>
-                    <FormControl>
-                      <Input
-                        className="border border-neutral-500 shadow-inner shadow-black"
-                        placeholder=""
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-
-            {/* text */}
-            <FormField
-                control={form.control}
-                name="Text"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="font-semibold">Text</FormLabel>
-                    <FormControl>
-                      <Textarea
-                        className="border row-span-2 border-neutral-500 w-full shadow-inner shadow-black"
-                        placeholder=""
-                        {...field}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            <Button
-              className="bg-[#B26C02] font-semibold shadow-md transition-all hover:bg-[#B26C02]/70 w- p-8 text-lg tracking-wid uppercase"
-              type="submit"
-            >
-              Enquire Now
-            </Button>
-          </form>
-        </Form>
-      </div>
+      <EnquireForm />
     </main>
   );
 };
